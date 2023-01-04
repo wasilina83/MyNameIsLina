02a42092ad83b1096e2beae927af4627ae28657c
wasilina83 <angelinaengelmann@live.de>
2023-01-04 12:55:43 +0000

    addit the code and created a json temp for new bullet points

--- src/MyNameIsEarl.cc
+++ src/MyNameIsEarl.cc
@@ -21,7 +21,8 @@ Als Beispiel folgende aufrufe 1 zu 1 im Terminal "hinschreiben":
     TEST
 Damit solltet ihr eine verändete .json erhalten. Viel Spaß!
 */
-int AddToMyList()
+
+/*int AddToMyList()
 {
     return 0;
 }
@@ -40,9 +41,28 @@ int ChangeMyList()
 {
     return 0;
 }
+*/
 
-int main(int argc, char** argv)
-{
-    std::cout << "Hey "
+int main(int argc, char** argv){
+    std::cout << "Hey du tragischer Titelheld dieser US-amerikanischen Comedyserie!" << "\n" << std::endl;
+    std::cout << "Lass uns doch mal sehen, was du alles auf dem Kerbholz hast." << "\n" << std::endl;
+    CLI::App app{"Füge deine Carma-Punkte hinzu mit: -r in src/TheList.json \n "};
+    std :: string filepath, inhalt, charmbalance, aendern, neuBulletPoint; //Strings für die  Optionen 
+    int activitynum; //Integer für things i've done and happened
+    int cathegoryynum; //Integer für bad und good karma oder good und bad things
+    app.add_option("-a, --add", neuBulletPoint, "Füge einen Neuen Bullet Point zur deiner Liste hinzu");
+    app.add_option("-A --activity", activitynum, "Activitynummer mit angeben, damit der neue Inhalt korrekt gespeichert wrid.\n "
+                                                    "(1) für things i've done. \n "
+                                                    "(2) things happened to me" );
+    app.add_option("-C --cathegory", cathegoryynum, "cathegorynummer mit angeben, damit der neue Inhalt korrekt gespeichert wrid. \n"
+                                                    "Bei things i've done: \n"
+                                                    " (1) für bad karma \n (2) good karma \n"
+                                                    "Bei things happened to me: \n"
+                                                    " (1) für good things \n (2) bad things\n");
+    app.add_option("-r, --read", filepath, "Path to config file" )
+        ->required()
+        ->check(CLI::ExistingFile);
+
+
 }
 
